c49f2ea8216b31aa3317f78450ab725be63710ef
vvalvvizal <vvalvvizal@gmail.com>
2023-10-06 13:58:23 +0900

    백승엽

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          바꿈 백승엽 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
